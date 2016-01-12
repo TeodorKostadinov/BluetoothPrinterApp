dd1b0b29bdb947c5ceed753f3eb2db2eca4ad9e4
Tihomir Krastev <tihomir.krastev@mail.bg>
2016-01-12 12:37:24 +0200

    theme color changed

--- BluetoothPrinterApp/app/src/main/java/com/inveitix/bluetoothprinterapp/BTDeviceList.java
+++ BluetoothPrinterApp/app/src/main/java/com/inveitix/bluetoothprinterapp/BTDeviceList.java
@@ -18,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class BTDeviceList extends ListActivity {
@@ -180,7 +181,6 @@ public class BTDeviceList extends ListActivity {
                                 if (btDeviceList.contains(device) == false) {
 
                                     btDevices.add(device);
-
                                     mArrayAdapter.add(device.getName() + "\n"
                                             + device.getAddress());
                                     mArrayAdapter.notifyDataSetInvalidated();
