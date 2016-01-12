cd3fee9f4cfc2a6d903734954ba2e97ec13ec22f
Tihomir Krastev <tihomir.krastev@mail.bg>
2016-01-12 15:33:50 +0200

    Web content downloaded in string, Marshmellow permissions added

--- BluetoothPrinterApp/app/src/main/java/com/inveitix/bluetoothprinterapp/MainActivity.java
+++ BluetoothPrinterApp/app/src/main/java/com/inveitix/bluetoothprinterapp/MainActivity.java
@@ -1,34 +1,58 @@
 package com.inveitix.bluetoothprinterapp;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+
 import android.bluetooth.BluetoothSocket;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    EditText message;
-    Button printbtn;
 
-    byte FONT_TYPE;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+public class MainActivity extends Activity {
+    private static final int PERMISSIONS_REQUEST_BLUETOOTH = 1;
+    private static final String TAG_REQUEST_PERMISSION = "Request permission";
+    private static final int PERMISSIONS_REQUEST_INTERNET = 0;
+    private static final int PERMISSIONS_REQUEST_BT_ADMIN = 2;
+    private static final int PERMISSIONS_REQUEST_LOCATION = 3;
     private static BluetoothSocket btsocket;
     private static OutputStream btoutputstream;
+    /**
+     * Called when the activity is first created.
+     */
+    EditText message;
+    Button printbtn;
+    byte FONT_TYPE;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        message = (EditText)findViewById(R.id.message);
-        printbtn = (Button)findViewById(R.id.printButton);
+        message = (EditText) findViewById(R.id.message);
+        printbtn = (Button) findViewById(R.id.printButton);
+        checkPermissions();
 
+        new FeedTask().execute();
         printbtn.setOnClickListener(new OnClickListener() {
 
             @Override
@@ -39,11 +63,10 @@ public class MainActivity extends Activity
     }
 
     protected void connect() {
-        if(btsocket == null){
+        if (btsocket == null) {
             Intent BTIntent = new Intent(getApplicationContext(), BTDeviceList.class);
             this.startActivityForResult(BTIntent, BTDeviceList.REQUEST_CONNECT_BT);
-        }
-        else{
+        } else {
 
             OutputStream opstream = null;
             try {
@@ -57,6 +80,7 @@ public class MainActivity extends Activity
         }
 
     }
+
     private void print_bt() {
         try {
             try {
@@ -67,7 +91,7 @@ public class MainActivity extends Activity
 
             btoutputstream = btsocket.getOutputStream();
 
-            byte[] printformat = { 0x1B, 0x21, FONT_TYPE };
+            byte[] printformat = {0x1B, 0x21, FONT_TYPE};
             btoutputstream.write(printformat);
             String msg = message.getText().toString();
             btoutputstream.write(msg.getBytes());
@@ -85,7 +109,7 @@ public class MainActivity extends Activity
     protected void onDestroy() {
         super.onDestroy();
         try {
-            if(btsocket!= null){
+            if (btsocket != null) {
                 btoutputstream.close();
                 btsocket.close();
                 btsocket = null;
@@ -95,17 +119,141 @@ public class MainActivity extends Activity
         }
     }
 
+    private boolean checkPermissions() {
+        int permissionCheck =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH);
+        int permissionInternet =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        int permissionBTAdmin =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN);
+        int permissionLocation =
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            message.setText(R.string.no_bluetooth_permissions);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.BLUETOOTH)) {
+                Toast.makeText(MainActivity.this, TAG_REQUEST_PERMISSION, Toast.LENGTH_SHORT).show();
+
+            } else {
+                requestBTPermission();
+            }
+            return false;
+        } else if (permissionInternet == PackageManager.PERMISSION_DENIED) {
+            message.setText(R.string.no_internet_permissions);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.INTERNET)) {
+                Toast.makeText(MainActivity.this, TAG_REQUEST_PERMISSION, Toast.LENGTH_SHORT).show();
+
+            } else {
+                requestInternetPermission();
+            }
+            return false;
+        } else if (permissionBTAdmin == PackageManager.PERMISSION_DENIED) {
+            message.setText(R.string.no_bt_admin_permissions);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.INTERNET)) {
+                Toast.makeText(MainActivity.this, TAG_REQUEST_PERMISSION, Toast.LENGTH_SHORT).show();
+
+            } else {
+                requestBTAdminPermission();
+            }
+            return false;
+        } else if (permissionLocation == PackageManager.PERMISSION_DENIED) {
+            message.setText(R.string.no_location_permissions);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                Toast.makeText(MainActivity.this, TAG_REQUEST_PERMISSION, Toast.LENGTH_SHORT).show();
+
+            } else {
+                requestLocationPermission();
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat
+                .requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSIONS_REQUEST_LOCATION);
+    }
+
+    private void requestBTAdminPermission() {
+        ActivityCompat
+                .requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_ADMIN},
+                        PERMISSIONS_REQUEST_BT_ADMIN);
+    }
+
+    private void requestInternetPermission() {
+        ActivityCompat
+                .requestPermissions(this, new String[]{Manifest.permission.INTERNET},
+                        PERMISSIONS_REQUEST_INTERNET);
+    }
+
+    private void requestBTPermission() {
+        ActivityCompat
+                .requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH},
+                        PERMISSIONS_REQUEST_BLUETOOTH);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         try {
             btsocket = BTDeviceList.getSocket();
-            if(btsocket != null){
+            if (btsocket != null) {
                 print_bt();
             }
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    class FeedTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpGet httpGet = new HttpGet("http://www.olx.bg");
+            HttpResponse response = null;
+            try {
+                response = httpClient.execute(httpGet, localContext);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String result = "";
+
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(
+                        new InputStreamReader(
+                                response.getEntity().getContent()
+                        )
+                );
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String line = null;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    result += line + "\n";
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            message.setText(s);
+            Log.e("Test", s);
         }
     }
 }
