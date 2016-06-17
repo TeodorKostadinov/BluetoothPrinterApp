81e72a38ef429f869a268065c937d429ca8cc334
Tihomir Krastev <tihomir.krastev@mail.bg>
2016-06-17 16:48:55 +0300

    first screen toolbar removed

--- app/src/main/java/com/inveitix/ticketprint/ui/UrlActivity.java
+++ app/src/main/java/com/inveitix/ticketprint/ui/UrlActivity.java
@@ -3,6 +3,7 @@ package com.inveitix.ticketprint.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,8 +30,12 @@ public class UrlActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_url);
 
+        setContentView(R.layout.activity_url);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
         ButterKnife.bind(this);
         sharedPref = this.getPreferences(Context.MODE_PRIVATE);
         checkBox.setChecked(load());
